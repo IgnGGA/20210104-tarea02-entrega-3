8c36c5c3ec412ab8f118ab11336a85cea8eef78d
Ignacio Gamboa <ign.gga@outlook.cl>
2021-01-05 11:31:27 -0300

    la tabla de info.vue quedo como un componente que se anida a info

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -4,124 +4,7 @@
       <div id="pageheader">
         <h1>NYSL Game Information</h1>
       </div>
-      <table class="table table-dark table-striped">
-        <thead>
-          <tr>
-            <th scope="col">SEPTEMBER</th>
-            <th scope="col">Teams</th>
-            <th scope="col">Location</th>
-            <th scope="col">Times</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row" rowspan="2">9/01</th>
-            <td>U1 and U4</td>
-            <td>AJ Katzenmaier</td>
-            <td>9:30 a.m.</td>
-          </tr>
-          <tr>
-            <td>U3 and U2</td>
-            <td>Greenbay</td>
-            <td>1:00 p.m.</td>
-          </tr>
-          <tr>
-            <th scope="row" rowspan="2">9/08</th>
-            <td>U5 and U6</td>
-            <td>Howard A Yeager</td>
-            <td>9:30 a.m.</td>
-          </tr>
-          <tr>
-            <td>U6 and U1</td>
-            <td>Marjorie P Hart</td>
-            <td>1:00 p.m.</td>
-          </tr>
-          <tr>
-            <th scope="row" rowspan="2">9/15</th>
-            <td>U2 and U4</td>
-            <td>North</td>
-            <td>9:30 a.m.</td>
-          </tr>
-          <tr>
-            <td>U3 and U5</td>
-            <td>AJ Katzenmaier</td>
-            <td>1:00 p.m.</td>
-          </tr>
-          <tr>
-            <th scope="row" rowspan="2">9/22</th>
-            <td>U1 and U3</td>
-            <td>South</td>
-            <td>9:30 a.m.</td>
-          </tr>
-          <tr>
-            <td>U2 and U6</td>
-            <td>Howard A Yeager</td>
-            <td>1:00 p.m.</td>
-          </tr>
-          <tr>
-            <th scope="row">9/29</th>
-            <td>U4 and U5</td>
-            <td>Greenbay</td>
-            <td>9:30 a.m.</td>
-          </tr>
-        </tbody>
-      </table>
-      <table class="table table-dark table-striped">
-        <thead>
-          <tr>
-            <th scope="col">OCTOBER</th>
-            <th scope="col">Teams</th>
-            <th scope="col">Location</th>
-            <th scope="col">Times</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row" rowspan="2">10/06</th>
-            <td>U2 and U5</td>
-            <td>Marjorie P Hart</td>
-            <td>9:30 a.m.</td>
-          </tr>
-          <tr>
-            <td>U1 and U6</td>
-            <td>South</td>
-            <td>1:00 p.m.</td>
-          </tr>
-          <tr>
-            <th scope="row" rowspan="2">10/08</th>
-            <td>U3 and U4</td>
-            <td>Howard A Yeager</td>
-            <td>9:30 a.m.</td>
-          </tr>
-          <tr>
-            <td>U5 and U1</td>
-            <td>Greenbay</td>
-            <td>1:00 p.m.</td>
-          </tr>
-           <tr>
-              <td scope="row" rowspan="2">10/20</td>
-              <td>U6 and U3</td>
-              <td>North</td>
-              <td>9:30 a.m.</td>
-            </tr>
-            <tr>
-              <td>U2 and U4</td>
-              <td>Marjorie P Hart</td>
-              <td>1:00 p.m.</td>
-            </tr>
-            <tr>
-              <td scope="row" rowspan="2">10/27</td>
-              <td>U3 and U1</td>
-              <td>AJ Katzenmaier</td>
-              <td>9:30 a.m.</td>
-            </tr>
-            <tr>
-              <td>U5 and U6</td>
-              <td>Howard A Yeager</td>
-              <td>1:00 p.m.</td>
-            </tr>
-        </tbody>
-      </table>
+        <Table></Table>
       <div id="bodycontent">
         <h2><em>Fall Schedule</em></h2>
         <p class="announcement">*All games take place on Saturday</p>
@@ -153,4 +36,12 @@
   </div>
 </template>
 
-       
+<script>
+import Table from '../components/Tabla.vue'
+export default {
+  name: 'Info',
+  components:{
+    Table
+  }
+}
+</script>     
