a3d1e04f6b81136930c338514e9b4056768a39ec
Ignacio Gamboa <ign.gga@outlook.cl>
2021-01-06 11:41:12 -0300

    Se referencia en main.js con la estructura de referencia de Tania, aparte se ejecuta el comando 'npm run serve' para obtener la carptea 'dist'

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+createApp(App).use(router).mount('#app')
+
 import Vue from 'vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.config.productionTip=false
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
-createApp(App).use(router).mount('#app')
+
+
+new Vue({
+    router,
+    render:h=>h(App)
+}).$mount('#app')
