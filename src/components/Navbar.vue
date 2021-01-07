d1d781758d2b7a7e2350cb4a3e619a3d1ca3e51e
Ignacio Gamboa <ign.gga@outlook.cl>
2021-01-06 21:58:59 -0300

    version creada con vue2.x donde si se pudo ejecutar con bootstrap-vue, boostrap sin problemas

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+<div id=Navbar>
+  <b-navbar toggleable type="dark" variant="dark">
+    <b-navbar-brand href="#">NYSL</b-navbar-brand>
+
+    <b-navbar-toggle target="navbar-toggle-collapse">
+      <template #default="{ expanded }">
+        <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
+        <b-icon v-else icon="chevron-bar-down"></b-icon>
+      </template>
+    </b-navbar-toggle>
+
+    <b-collapse id="navbar-toggle-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item to="/">Home</b-nav-item>
+        <b-nav-item href="/about">About</b-nav-item>
+        <b-nav-item href="/contact">Contact</b-nav-item>
+        <b-nav-item href="/info">Info</b-nav-item>
+        <b-nav-item href="/registration">Registratin</b-nav-item>
+        <b-nav-item href="/rules">Rules</b-nav-item>
+        <b-nav-item href="#" disabled>NorthSide Youth Soccer League</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+  <router-view />
+</div>
+</template>
