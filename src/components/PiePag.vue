f004a6394ec64a97b350971a23aae404da767691
Ignacio Gamboa <ign.gga@outlook.cl>
2021-01-07 12:46:37 -0300

    se agregan fotos al boton momentos

--- src/components/PiePag.vue
+++ src/components/PiePag.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="PiePag">
-    <b-button v-b-toggle.sidebar-footer>Toggle Sidebar</b-button>
+    <b-button v-b-toggle.sidebar-footer>Momentos</b-button>
     <b-sidebar id="sidebar-footer" aria-label="Sidebar with custom footer" no-header shadow>
       <template #footer="{ hide }">
        <div class="d-flex bg-dark text-light align-items-center px-3 py-2">
-        <strong class="mr-auto">Footer</strong>
+        <strong class="mr-auto">NorthSide Youth Socer League</strong>
         <b-button size="sm" @click="hide">Close</b-button>
        </div>
       </template>
@@ -13,8 +13,19 @@
           Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
           in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
         </p>
-        <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+        <Carrusel></Carrusel>
       </div>
     </b-sidebar>
   </div>
 </template>
+
+<script>
+import Carrusel from './Carrusel.vue'
+
+export default {
+  name:'PiePag',
+  components:{
+    Carrusel,
+  }
+}
+</script>
